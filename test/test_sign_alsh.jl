54b1be1d8044b41ec7a4fb438d26242b9c4c05a6
Will Shand <wish5031@colorado.edu>
2019-07-26 02:29:07 -0500

    Fixed a bug in the manual SignALSH test computations where we forgot to divide the computed norms by the maximum norm for h(P(x)). Added some tests to ensure that SignALSH actually generates hash collisions for vectors with large inner products.

--- test/test_sign_alsh.jl
+++ test/test_sign_alsh.jl
@@ -22,14 +22,55 @@ using Test, Random, LSH, LinearAlgebra
 		# 1. Compute the indexing hashes manually
 		norms = map(norm, eachcol(X))
 		maxnorm = maximum(norms)
+		norms .*= 1 / maxnorm
+		Y = X ./ maxnorm
+
+		@test all(map(norm,eachcol(Y)) .≤ 1)
+		@test all(0 .≤ norms .≤ 1)
+
 		norm_powers = -[norms.^2 norms.^4 norms.^8] .+ 1/2
-		Px = [X ./ maxnorm; norm_powers']
+		Px = [Y; norm_powers']
 
 		@test simhash(Px) == ihashes
 
-		# 2. Compute the query hashes manually
+		# 2. Compute the querying hashes manually
 		Qx = [X ./ norms'; zeros(1,128); zeros(1,128); zeros(1,128)]
 
 		@test simhash(Qx) == qhashes
+	end
+
+	@testset "SignALSH generates collisions for large inner products" begin
+		input_length = 5; n_hashes = 128;
+		hashfn = SignALSH(input_length, n_hashes)
+
+		x = randn(input_length)
+		x_query_hashes = query_hash(hashfn, x)
+
+		# Check that SignALSH isn't just generating a single query hash
+		@test any(x_query_hashes .!= x_query_hashes[1])
+
+		# Compute the indexing hashes for a dataset with four vectors:
+		# a) 10 * x (where x is the test query vector)
+		# b) x
+		# c) A vector of all zeros
+		# d) -x
+		dataset = [(10*x) x zeros(input_length) -x]
+		p_hashes = index_hash(hashfn, dataset)
+
+		# Each collection of hashes should be different from one another
+		@test let result = true
+			for (ii,jj) in product(1:4, 1:4)
+				if ii != jj && p_hashes[:,ii] == p_hashes[:,jj]
+					result = false
+					break
+				end
+			end
+			result
+		end
+
+		# The number of collisions should be highest for x and 2*x, second-highest
+		# for x and x, second-lowest for x and zeros, and lowest for x and -x
+		n_collisions = [sum(x_query_hashes .== p) for p in eachcol(p_hashes)]
+		@test n_collisions[1] > n_collisions[2] > n_collisions[3] > n_collisions[4]
 	end
 end
