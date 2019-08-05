a301e18e0ddb3a7fda283acbd1bb1c3e970c34ca
Will Shand <wish5031@colorado.edu>
2019-08-05 11:36:45 -0500

    Moved hash function implementations into the hashes/ directory.

--- src/LSH.jl
+++ src/LSH.jl
@@ -4,10 +4,12 @@ using Distributions, LinearAlgebra, SparseArrays
 
 include("utils.jl")
 include("LSHBase.jl")
-include("simhash.jl")
-include("lphash.jl")
-include("mips_hash.jl")
-include("sign_alsh.jl")
+
+# Hash functions
+include(joinpath("hashes", "simhash.jl"))
+include(joinpath("hashes", "lphash.jl"))
+include(joinpath("hashes", "mips_hash.jl"))
+include(joinpath("hashes", "sign_alsh.jl"))
 
 export SimHash, LpHash, L1Hash, L2Hash, MIPSHash,
 	SignALSH, hashtype, index_hash, query_hash, n_hashes
