6af90652a5bb4b95f38f9e493c5d7d29a46ae0bb
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-01-06 01:13:14 -0700

    Make some readability improvements to mips_hash.jl by using @generated functions instead of large multiple dispatch chains, and using better variable names.

--- src/hashes/mips_hash.jl
+++ src/hashes/mips_hash.jl
@@ -14,12 +14,12 @@ Asymmetric LSH for approximate maximum inner product search. Ref:
 	https://arxiv.org/abs/1405.5869
 """
 struct MIPSHash{T <: Union{Float32,Float64}} <: AsymmetricLSHFunction
-	coeff_A :: Matrix{T}
-	coeff_B :: Matrix{T}
-	denom :: T
-	shift :: Vector{T}
-	Qshift :: Vector{T}
-	m :: Integer
+    coeff_A :: Matrix{T}
+    coeff_B :: Matrix{T}
+    denom :: T
+    shift :: Vector{T}
+    Qshift :: Vector{T}
+    m :: Integer
 end
 
 ### External MIPSHash constructors
@@ -54,47 +54,54 @@ mat(x::AbstractMatrix) = x
 h(P(x)) definitions
 =========#
 
-MIPSHash_P(h::MIPSHash{T}, x::AbstractArray{<:Real}; kws...) where {T <: Union{Float32,Float64}} =
-	MIPSHash_P(h, T.(x); kws...)
+@generated function MIPSHash_P(
+        hashfn::MIPSHash{T},
+        x::AbstractArray{S}) where {T,S}
 
-MIPSHash_P(h :: MIPSHash{T}, x :: AbstractArray{T}; kws...) where {T <: Union{Float32,Float64}} =
-	invoke(MIPSHash_P, Tuple{MIPSHash{T}, AbstractArray}, h, x; kws...)
-
-MIPSHash_P(h :: MIPSHash{T}, x :: AbstractVector{T}; kws...) where {T <: Union{Float32,Float64}} =
-	invoke(MIPSHash_P, Tuple{MIPSHash{T}, AbstractArray}, h, x; kws...) |> vec
-
-function MIPSHash_P(h :: MIPSHash{T}, x :: AbstractArray) where {T}
-	norms = col_norms(x)
-	maxnorm = maximum(norms)
-	maxnorm = maxnorm == 0 ? 1 : maxnorm	# To handle some edge cases
-	BLAS.scal!(length(norms), 1/maxnorm, norms, 1)
-
-	# First, perform a matvec on x and the first array of coefficients.
-	# Note: aTx is an n_hashes × n_inputs array
-	aTx = h.coeff_A * x .* (1/maxnorm) |> mat
-
-	# Compute norms^2, norms^4, ... norms^(2^m).
-	# Multiply these by the second array of coefficients and add them to aTx, so
-	# that in totality we compute
-	#
-	# 		aTx = [coeff_A, coeff_B] * P(x)
-	# 			= [coeff_A, coeff_B] * [x; norms^2; ...; norms^(2^m)]
-	#
-	# By making these computations in a somewhat roundabout way (rather than following
-	# the formula above), we save a lot of memory by avoiding concatenations.
-	# Note that m is typically small, so these iterations don't do much to harm performance
-	for ii = 1:h.m
-		norms .^= 2
-		MIPSHash_P_update_aTx!(h.coeff_B[:,ii], norms, aTx)
-	end
-
-	# Compute the remainder of the hash the same way we'd compute an L^p distance LSH.
-	@. aTx = aTx / h.denom + h.shift
-
-	return floor.(Int32, aTx)
+    if T != S
+        # Perform type conversion to hit BLAS
+        :( MIPSHash_P(hashfn, T.(x)) )
+    elseif x <: AbstractVector
+        :( _MIPSHash_P(hashfn, x) |> vec )
+    else
+        :( _MIPSHash_P(hashfn, x) )
+    end
 end
 
-MIPSHash_P_update_aTx!(coeff :: Vector{T}, norms :: Vector{T}, aTx :: Array{T}) where T =
+function _MIPSHash_P(h :: MIPSHash{T}, x :: AbstractArray) where {T}
+    norms = col_norms(x)
+    maxnorm = maximum(norms)
+    maxnorm = maxnorm == 0 ? 1 : maxnorm	# To handle some edge cases
+    BLAS.scal!(length(norms), 1/maxnorm, norms, 1)
+
+    # First, perform a matvec on x and the first array of coefficients.
+    # Note: aTx is an n_hashes × n_inputs array
+    aTx = h.coeff_A * x .* (1/maxnorm) |> mat
+
+    # Compute norms^2, norms^4, ... norms^(2^m).
+    # Multiply these by the second array of coefficients and add them to aTx, so
+    # that in totality we compute
+    #
+    # 		aTx = [coeff_A, coeff_B] * P(x)
+    # 			= [coeff_A, coeff_B] * [x; norms^2; ...; norms^(2^m)]
+    #
+    # By making these computations in a somewhat roundabout way (rather than
+    # following the formula above), we save a lot of memory by avoiding
+    # concatenations.
+    # Note that m is typically small, so these iterations don't do much to harm
+    # performance
+    for ii = 1:h.m
+        norms .^= 2
+        MIPSHash_P_update_aTx!(h.coeff_B[:,ii], norms, aTx)
+    end
+
+    # Compute the remainder of the hash the same way we'd compute an L^p distance LSH.
+    @. aTx = aTx / h.denom + h.shift
+
+    return floor.(Int32, aTx)
+end
+
+MIPSHash_P_update_aTx!(coeff::Vector{T}, norms::Vector{T}, aTx :: Array{T}) where T =
 	BLAS.ger!(T(1), coeff, norms, aTx)
 
 MIPSHash_P_update_aTx!(coeff, norms, aTx) =
@@ -103,50 +110,57 @@ MIPSHash_P_update_aTx!(coeff, norms, aTx) =
 #==========
 h(Q(x)) definitions
 ===========#
-function MIPSHash_Q(h :: MIPSHash, x :: AbstractArray)
-	# First, perform a matvec on x and the first array of coefficients.
-	# Note: aTx is an n_hashes × n_inputs array
-	aTx = h.coeff_A * x |> mat
 
-	# Normalize the query vectors. We perform normalization after computing
-	# aTx (rather than before) so that we don't have to allocate a new array
-	# of size(x). Moreover, for large input vectors, the size of aTx is typically
-	# much smaller than the size of x.
-	f(x :: T) where {T} = (x ≈ T(0) ? T(1) : x)
-	norms = col_norms(x)
-	map!(f, norms, norms)
+@generated function MIPSHash_Q(
+        hashfn::MIPSHash{T},
+        x::AbstractArray{S}) where {T,S}
 
-	aTx .= aTx ./ norms'
-
-	# Here, we would multiply the second array of coefficients by the elements that
-	# Q(x) concatenates to x. Then we'd add this to aTx so that in total we compute
-	#
-	#		aTx = [coeff_A, coeff_B] * Q(x)
-	#			= [coeff_A, coeff_B] * [x; 1/2; 1/2; ...; 1/2]
-	#
-	# Then we'd proceed with computing the rest of the L^2 distance LSH. However,
-	# since the values concatenated on by Q(x) are always the same, we actually
-	# pre-compute coeff_B * [1/2; 1/2; ...; 1/2] + shift when we construct the
-	# MIPSHash to reduce the number of computations.
-	@. aTx = aTx / h.denom + h.Qshift
-
-	return floor.(Int32, aTx)
+    if T != S
+        # Perform type conversion to hit BLAS
+        :( MIPSHash_Q(hashfn, T.(x)) )
+    elseif x <: AbstractVector
+        :( _MIPSHash_Q(hashfn, x) |> vec )
+    else
+        :( _MIPSHash_Q(hashfn, x) )
+    end
 end
 
-MIPSHash_Q(h :: MIPSHash{T}, x :: AbstractArray{<:Real}) where {T <: Union{Float32,Float64}} =
-	MIPSHash_Q(h, T.(x))
+function _MIPSHash_Q(hashfn::MIPSHash, x::AbstractArray)
+    # First, perform a matvec on x and the first array of coefficients.
+    # Note: aTx is an n_hashes × n_inputs array
+    aTx = hashfn.coeff_A * x |> mat
 
-MIPSHash_Q(h :: MIPSHash{T}, x :: AbstractArray{T}) where {T <: Union{Float32,Float64}} =
-	invoke(MIPSHash_Q, Tuple{MIPSHash{T}, AbstractArray}, h, x)
+    # Normalize the query vectors. We perform normalization after computing
+    # aTx (rather than before) so that we don't have to allocate a new array
+    # of size(x). Moreover, for large input vectors, the size of aTx is typically
+    # much smaller than the size of x.
+    f(x::T) where {T} = (x ≈ T(0) ? T(1) : x)
+    norms = col_norms(x)
+    map!(f, norms, norms)
 
-MIPSHash_Q(h :: MIPSHash{T}, x :: AbstractVector{T}) where {T <: Union{Float32,Float64}} =
-	invoke(MIPSHash_Q, Tuple{MIPSHash{T}, AbstractArray}, h, x) |> vec
+    aTx .= aTx ./ norms'
+
+    # Here, we would multiply the second array of coefficients by the elements
+    # that Q(x) concatenates to x. Then we'd add this to aTx so that in total we
+    # compute
+    #
+    #		aTx = [coeff_A, coeff_B] * Q(x)
+    #			= [coeff_A, coeff_B] * [x; 1/2; 1/2; ...; 1/2]
+    #
+    # Then we'd proceed with computing the rest of the L^2 distance LSH. However,
+    # since the values concatenated on by Q(x) are always the same, we actually
+    # pre-compute coeff_B * [1/2; 1/2; ...; 1/2] + shift when we construct the
+    # MIPSHash to reduce the number of computations.
+    @. aTx = aTx / hashfn.denom + hashfn.Qshift
+
+    return floor.(Int32, aTx)
+end
 
 #========================
 LSHFunction and AsymmetricLSHFunction API compliance
 ========================#
-index_hash(h :: MIPSHash, x) = MIPSHash_P(h, x)
-query_hash(h :: MIPSHash, x) = MIPSHash_Q(h, x)
+index_hash(hashfn::MIPSHash, x) = MIPSHash_P(hashfn, x)
+query_hash(hashfn::MIPSHash, x) = MIPSHash_Q(hashfn, x)
 
-n_hashes(h :: MIPSHash) = length(h.shift)
-hashtype(:: MIPSHash) = Vector{Int32}
+n_hashes(hashfn::MIPSHash) = length(hashfn.shift)
+hashtype(::MIPSHash) = Vector{Int32}
