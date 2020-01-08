dbe90c6910876b63d818d3b66d1c5873b1216e27
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-01-07 23:45:19 -0700

    Update the SignALSH API (ref: issue #1).
    Change the SignALSH API so that new hash functions are constructed in the same way as other hash functions in the LSH module -- i.e., its constructor has the signature SignALSH(number of hash functions; family-specific keyword params). This is one of two commits to address issue #1.
    
    Squashed commit of the following:
    
    commit 755963ffbc7a5450c9eb3c25e92691c4178cc65a
    Author: kernelmethod <17100608+kernelmethod@users.noreply.github.com>
    Date:   Tue Jan 7 23:45:07 2020 -0700
    
        Fix test_table.jl so that the tests that use SignALSH now use the updated constructor.
    
    commit 8345ed57d523701a1d3cd1518f073cc324325438
    Author: kernelmethod <17100608+kernelmethod@users.noreply.github.com>
    Date:   Tue Jan 7 23:41:20 2020 -0700
    
        Update the SignALSH API so that it matches the general LSHFunction API (i.e., it's constructed with SignALSH(n_hashes; kws...)).
    
    commit ab1cdb71f525a96bc40f862c047fce1d1838ffa3
    Author: kernelmethod <17100608+kernelmethod@users.noreply.github.com>
    Date:   Tue Jan 7 23:14:10 2020 -0700
    
        Modify test_sign_alsh.jl for the new SignALSH.jl API.

--- src/hashes/sign_alsh.jl
+++ src/hashes/sign_alsh.jl
@@ -14,31 +14,59 @@ Typedefs
 Implementation of the SignALSH maximum inner product search (MIPS)
 hash function. Ref:
 
-	https://arxiv.org/abs/1410.5410v2
+    https://arxiv.org/abs/1410.5410v2
 """
-struct SignALSH{T <: Union{Float32,Float64}} <: AsymmetricLSHFunction
-
+mutable struct SignALSH{T <: Union{Float32,Float64}} <: AsymmetricLSHFunction
     coeff_A :: Matrix{T}
     coeff_B :: Matrix{T}
     P_shift :: Vector{T}
     m :: Int64
+
+    # Whether or not SignALSH should round up to the next power of 2 when
+    # resizing its coefficient array.
+    resize_pow2 :: Bool
 end
 
 ### External SignALSH constructors
-function SignALSH{T}(
-        input_length::Integer,
-        n_hashes::Integer,
-        m::Integer = 3) where {T}
+function SignALSH{T}(n_hashes::Integer;
+                     m::Integer = 3,
+                     resize_pow2::Bool = false) where {T}
 
-    coeff_A = randn(T, n_hashes, input_length)
+    coeff_A = Matrix{T}(undef, n_hashes, 0)
     coeff_B = randn(T, n_hashes, m)
     P_shift = coeff_B * fill(T(1/2), m)
 
-    SignALSH(coeff_A, coeff_B, P_shift, Int64(m))
+    SignALSH(coeff_A, coeff_B, P_shift, Int64(m), resize_pow2)
 end
 
-SignALSH(args...; kws...) =
-	SignALSH{Float32}(args...; kws...)
+SignALSH(args...; dtype=Float32, kws...) =
+    SignALSH{dtype}(args...; kws...)
+
+#============
+SignALSH helper functions
+=============#
+
+function Base.resize!(hashfn::SignALSH{T}, n::Integer) where T
+    n = (hashfn.resize_pow2) ? nextpow(2, n) : n
+
+    # Note: the only field of SignALSH that is dependent on the input size is
+    # coeff_A
+    n_hashes, old_n = size(hashfn.coeff_A)
+    old_coeff_A = hashfn.coeff_A
+    new_coeff_A = similar(old_coeff_A, n_hashes, n)
+
+    new_coeff_A[1:end,1:min(n,old_n)] .= old_coeff_A
+
+    if n > old_n
+        new_coeff_slice = @views new_coeff_A[1:end,old_n+1:end]
+        @views map!(x -> randn(T), new_coeff_slice, new_coeff_slice)
+    end
+
+    hashfn.coeff_A = new_coeff_A
+end
+
+current_max_input_size(hashfn::SignALSH) =
+    size(hashfn.coeff_A, 2)
 
 #============
 Hash computation implementation
@@ -47,87 +75,90 @@ Hash computation implementation
 #=
 h(P(x)) definitions
 =#
-SignALSH_P(h :: SignALSH{T}, x :: AbstractArray{<:Real}) where {T<:Union{Float32,Float64}} =
-	SignALSH_P(h, T.(x))
+SignALSH_P(hashfn::SignALSH{T}, x::AbstractArray) where T =
+    SignALSH_P(hashfn, T.(x))
 
-SignALSH_P(h :: SignALSH{T}, x :: AbstractArray{T}) where {T<:Union{Float32,Float64}} =
-	invoke(SignALSH_P, Tuple{SignALSH{T},AbstractArray}, h, x)
+function SignALSH_P(hashfn::SignALSH{T}, x::AbstractArray{T}) where {T}
+    # SignALSH_P is essentially just SimHash on
+    #
+    #	P(x) = [x; 1/2-norms^2; 1/2-norms^4; ... 1/2-norms^(2^m)]
+    #
+    # after dividing through x by the largest norm of all of the columns
+    # of x.
+    norms = col_norms(x)
+    maxnorm = maximum(norms)
+    maxnorm = maxnorm == 0 ? 1 : maxnorm	# To handle some edge cases
+    norms .*= 1/maxnorm
 
-function SignALSH_P(h :: SignALSH{T}, x :: AbstractArray) where {T}
-	# SignALSH_P is essentially just SimHash on
-	#
-	#	P(x) = [x; 1/2-norms^2; 1/2-norms^4; ... 1/2-norms^(2^m)]
-	#
-	# after dividing through x by the largest norm of all of the columns
-	# of x.
-	norms = col_norms(x)
-	maxnorm = maximum(norms)
-	maxnorm = maxnorm == 0 ? 1 : maxnorm	# To handle some edge cases
-	norms .*= 1/maxnorm
+    n = size(x,1)
+    if n > current_max_input_size(hashfn)
+        resize!(hashfn, n)
+    end
 
-	Ax = h.coeff_A * x .* (1/maxnorm)
+    Ax = @views hashfn.coeff_A[1:end,1:n] * x .* (1/maxnorm)
 
-	# Perform the transformation P(x) on x, except that instead of actually
-	# allocating memory for it and computing it, pile it onto Ax
-	@. Ax += h.P_shift
+    # Perform the transformation P(x) on x, except that instead of actually
+    # allocating memory for it and computing it, pile it onto Ax
+    @. Ax += hashfn.P_shift
 
-	for ii = 1:h.m
-		# Iteratively compute norms^2, norms^4, ... norms^(2^m). Multiply them by
-		# columns of coeff_B and add to aTx, so that we end up with
-		#
-		# 	Ax = [coeff_A, coeff_B] * P(x)
-		# 		= [coeff_A, coeff_B] * [x; 1/2-norms^2; ...; 1/2-norms^(2^m)]
-		#
-		# Note that we don't need to account for the 1/2 terms, which are accounted
-		# for by the earlier addition of P_shift.
-		norms .^= 2
-		SignALSH_P_update_Ax!(h.coeff_B[:,ii], norms, Ax)
-	end
+    for ii = 1:hashfn.m
+        # Iteratively compute norms^2, norms^4, ... norms^(2^m). Multiply them by
+        # columns of coeff_B and add to aTx, so that we end up with
+        #
+        # 	Ax = [coeff_A, coeff_B] * P(x)
+        # 		= [coeff_A, coeff_B] * [x; 1/2-norms^2; ...; 1/2-norms^(2^m)]
+        #
+        # Note that we don't need to account for the 1/2 terms, which are
+        # accounted for by the earlier addition of P_shift.
+        norms .^= 2
+        SignALSH_P_update_Ax!(hashfn.coeff_B[:,ii], norms, Ax)
+    end
 
-	return Ax .≥ 0
+    return Ax .≥ 0
 end
 
-SignALSH_P_update_Ax!(coeff :: Vector{T}, norms :: Vector{T}, Ax :: Array{T}) where T =
+SignALSH_P_update_Ax!(coeff::Vector{T}, norms::Vector{T}, Ax::Array{T}) where T =
 	BLAS.ger!(T(-1), coeff, norms, Ax)
 
 # When the coefficients or norms are AbstractVectors, cascade through to reshape
 # them into AbstractMatrix before updating Ax.
-SignALSH_P_update_Ax!(coeff :: AbstractVector, norms :: AbstractVector, Ax) =
-	SignALSH_P_update_Ax!(reshape(coeff, length(coeff), 1), norms, Ax)
+SignALSH_P_update_Ax!(coeff::AbstractVector, norms::AbstractVector, Ax) =
+    SignALSH_P_update_Ax!(reshape(coeff, length(coeff), 1), norms, Ax)
 
-SignALSH_P_update_Ax!(coeff, norms :: AbstractVector, Ax) =
-	SignALSH_P_update_Ax!(coeff, reshape(norms, length(norms), 1), Ax)
+SignALSH_P_update_Ax!(coeff, norms::AbstractVector, Ax) =
+    SignALSH_P_update_Ax!(coeff, reshape(norms, length(norms), 1), Ax)
 
 SignALSH_P_update_Ax!(coeff, norms, Ax) =
-	(Ax .-= coeff * norms')
+    (Ax .-= coeff * norms')
 
 #=
 h(Q(x)) definitions
 =#
-function SignALSH_Q(h :: SignALSH{T}, x :: AbstractArray) where {T}
-	Ax = h.coeff_A * x
-	norms = col_norms(x)
-	map!(inv, norms, norms)
-	@. Ax * norms' ≥ T(0)
+
+SignALSH_Q(hashfn::SignALSH{T}, x::AbstractArray) where T =
+    SignALSH_Q(hashfn, T.(x))
+
+SignALSH_Q(hashfn::SignALSH{T}, x::AbstractVector{T}) where T =
+    invoke(SignALSH_Q, Tuple{SignALSH{T},AbstractArray{T}}, hashfn, x) |> vec
+
+function SignALSH_Q(hashfn::SignALSH{T}, x::AbstractArray{T}) where {T}
+    n = size(x,1)
+
+    if n > current_max_input_size(hashfn)
+        resize!(hashfn, n)
+    end
+
+    Ax = @views hashfn.coeff_A[1:end,1:n] * x
+    norms = col_norms(x)
+    map!(inv, norms, norms)
+    @. Ax * norms' ≥ T(0)
 end
-
-SignALSH_Q(h :: SignALSH{T}, x :: AbstractArray{<:Real}) where {T<:Union{Float32,Float64}} =
-	SignALSH_Q(h, T.(x))
-
-SignALSH_Q(h :: SignALSH{T}, x :: AbstractArray{T}) where {T <: Union{Float32,Float64}} =
-	invoke(SignALSH_Q, Tuple{SignALSH{T},AbstractArray}, h, x)
-
-SignALSH_Q(h :: SignALSH{T}, x :: AbstractVector{T}) where {T <: Union{Float32,Float64}} =
-	invoke(SignALSH_Q, Tuple{SignALSH{T},AbstractArray}, h, x) |> vec
 
 #========================
 LSHFunction and AsymmetricLSHFunction API compliance
 ========================#
-index_hash(h :: SignALSH, x) = SignALSH_P(h, x)
-query_hash(h :: SignALSH, x) = SignALSH_Q(h, x)
+index_hash(h::SignALSH, x) = SignALSH_P(h, x)
+query_hash(h::SignALSH, x) = SignALSH_Q(h, x)
 
-n_hashes(h :: SignALSH) = size(h.coeff_A, 1)
-hashtype(:: SignALSH) = BitArray{1}
-
-function redraw!(h :: SignALSH{T}) where {T}
-end
+n_hashes(h::SignALSH) = size(h.coeff_A, 1)
+hashtype(::SignALSH) = BitArray{1}
--- test/hashes/test_sign_alsh.jl
+++ test/hashes/test_sign_alsh.jl
@@ -6,110 +6,110 @@ include(joinpath("..", "utils.jl"))
 Tests
 ==================#
 @testset "SignALSH tests" begin
-	Random.seed!(RANDOM_SEED)
+    Random.seed!(RANDOM_SEED)
 
-	@test_skip @testset "Can hash inputs correctly with SignALSH" begin
-		input_length = 5
-		n_hashes = 8
+    @test_skip @testset "Can hash inputs correctly with SignALSH" begin
+        input_length = 5
+        n_hashes = 8
 
-		hashfn = SignALSH(input_length, n_hashes, 3)
-		simhash = SimHash([hashfn.coeff_A hashfn.coeff_B])
-		
-		# By default, hashfn should be using Float32 for its coefficients
-		@test isa(hashfn, SignALSH{Float32})
+        hashfn = SignALSH(n_hashes; m=3)
+        simhash = SimHash([hashfn.coeff_A hashfn.coeff_B])
 
-		X = rand(Float32, input_length, 128)
-		ihashes = index_hash(hashfn, X)
-		qhashes = query_hash(hashfn, X)
+        # By default, hashfn should be using Float32 for its coefficients
+        @test isa(hashfn, SignALSH{Float32})
 
-		@test eltype(ihashes) == eltype(qhashes)
-		@test BitArray{1} == hashtype(hashfn)
+        X = rand(Float32, input_length, 128)
+        ihashes = index_hash(hashfn, X)
+        qhashes = query_hash(hashfn, X)
 
-		# 1. Compute the indexing hashes manually
-		norms = map(norm, eachcol(X))
-		maxnorm = maximum(norms)
-		norms .*= 1 / maxnorm
-		Y = X ./ maxnorm
+        @test eltype(ihashes) == eltype(qhashes)
+        @test BitArray{1} == hashtype(hashfn)
 
-		@test all(map(norm,eachcol(Y)) .≤ 1)
-		@test all(0 .≤ norms .≤ 1)
+        # 1. Compute the indexing hashes manually
+        norms = map(norm, eachcol(X))
+        maxnorm = maximum(norms)
+        norms .*= 1 / maxnorm
+        Y = X ./ maxnorm
 
-		norm_powers = -[norms.^2 norms.^4 norms.^8] .+ 1/2
-		Px = [Y; norm_powers']
+        @test all(map(norm,eachcol(Y)) .≤ 1)
+        @test all(0 .≤ norms .≤ 1)
 
-		@test simhash(Px) == ihashes
+        norm_powers = -[norms.^2 norms.^4 norms.^8] .+ 1/2
+        Px = [Y; norm_powers']
 
-		# 2. Compute the querying hashes manually
-		Qx = [X ./ norms'; zeros(1,128); zeros(1,128); zeros(1,128)]
+        @test simhash(Px) == ihashes
 
-		@test simhash(Qx) == qhashes
-	end
+        # 2. Compute the querying hashes manually
+        Qx = [X ./ norms'; zeros(1,128); zeros(1,128); zeros(1,128)]
 
-	@testset "SignALSH generates collisions for large inner products" begin
-		input_length = 5; n_hashes = 128;
-		hashfn = SignALSH(input_length, n_hashes)
+        @test simhash(Qx) == qhashes
+    end
 
-		x = randn(input_length)
-		x_query_hashes = query_hash(hashfn, x)
+    @testset "SignALSH generates collisions for large inner products" begin
+        input_length = 5; n_hashes = 128;
+        hashfn = SignALSH(n_hashes)
 
-		# Check that SignALSH isn't just generating a single query hash
-		@test any(x_query_hashes .!= x_query_hashes[1])
+        x = randn(input_length)
+        x_query_hashes = query_hash(hashfn, x)
 
-		# Compute the indexing hashes for a dataset with four vectors:
-		# a) 10 * x (where x is the test query vector)
-		# b) x
-		# c) A vector of all zeros
-		# d) -x
-		dataset = [(10*x) x zeros(input_length) -x]
-		p_hashes = index_hash(hashfn, dataset)
+        # Check that SignALSH isn't just generating a single query hash
+        @test any(x_query_hashes .!= x_query_hashes[1])
 
-		# Each collection of hashes should be different from one another
-		@test let result = true
-			for (ii,jj) in product(1:4, 1:4)
-				if ii != jj && p_hashes[:,ii] == p_hashes[:,jj]
-					result = false
-					break
-				end
-			end
-			result
-		end
+        # Compute the indexing hashes for a dataset with four vectors:
+        # a) 10 * x (where x is the test query vector)
+        # b) x
+        # c) A vector of all zeros
+        # d) -x
+        dataset = [(10*x) x zeros(input_length) -x]
+        p_hashes = index_hash(hashfn, dataset)
 
-		# The number of collisions should be highest for x and 2*x, second-highest
-		# for x and x, second-lowest for x and zeros, and lowest for x and -x
-		n_collisions = [sum(x_query_hashes .== p) for p in eachcol(p_hashes)]
-		@test n_collisions[1] > n_collisions[2] > n_collisions[3] > n_collisions[4]
-	end
+        # Each collection of hashes should be different from one another
+        @test let result = true
+            for (ii,jj) in Iterators.product(1:4, 1:4)
+	            if ii != jj && p_hashes[:,ii] == p_hashes[:,jj]
+		            result = false
+		            break
+	            end
+            end
+            result
+        end
 
-	@testset "Can hash sparse arrays" begin
-		input_size = 100
-		n_inputs = 150
-		n_hashes = 2
+        # The number of collisions should be highest for x and 2*x, second-highest
+        # for x and x, second-lowest for x and zeros, and lowest for x and -x
+        n_collisions = [sum(x_query_hashes .== p) for p in eachcol(p_hashes)]
+        @test n_collisions[1] > n_collisions[2] > n_collisions[3] > n_collisions[4]
+    end
 
-		hashfn = SignALSH(input_size, n_hashes)
-		x = sprandn(input_size, n_inputs, 0.2)
+    @testset "Can hash sparse arrays" begin
+        input_size = 100
+        n_inputs = 150
+        n_hashes = 2
 
-		# Mostly just need to test that the following lines don't crash
-		ih = index_hash(hashfn, x)
-		qh = query_hash(hashfn, x)
+        hashfn = SignALSH(n_hashes)
+        x = sprandn(input_size, n_inputs, 0.2)
 
-		@test size(ih) == (n_hashes, n_inputs)
-		@test size(qh) == (n_hashes, n_inputs)
-	end
+        # Mostly just need to test that the following lines don't crash
+        ih = index_hash(hashfn, x)
+        qh = query_hash(hashfn, x)
 
-	@testset "Can hash matrix adjoints" begin
-		input_size = 100
-		n_inputs = 150
-		n_hashes = 2
-		hashfn = SignALSH(input_size, n_hashes)
+        @test size(ih) == (n_hashes, n_inputs)
+        @test size(qh) == (n_hashes, n_inputs)
+    end
 
-		## Test 1: regular matrix adjoint
-		x = randn(n_inputs, input_size)'
-		@test index_hash(hashfn, x) == index_hash(hashfn, copy(x))
-		@test query_hash(hashfn, x) == query_hash(hashfn, copy(x))
+    @testset "Can hash matrix adjoints" begin
+        input_size = 100
+        n_inputs = 150
+        n_hashes = 2
+        hashfn = SignALSH(n_hashes)
 
-		## Test 2: sparse matrix adjoint
-		x = sprandn(n_inputs, input_size, 0.2)'
-		@test index_hash(hashfn, x) == index_hash(hashfn, copy(x))
-		@test query_hash(hashfn, x) == query_hash(hashfn, copy(x))
-	end
+        ## Test 1: regular matrix adjoint
+        x = randn(n_inputs, input_size)'
+        @test index_hash(hashfn, x) == index_hash(hashfn, copy(x))
+        @test query_hash(hashfn, x) == query_hash(hashfn, copy(x))
+
+        ## Test 2: sparse matrix adjoint
+        x = sprandn(n_inputs, input_size, 0.2)'
+        @test index_hash(hashfn, x) == index_hash(hashfn, copy(x))
+        @test query_hash(hashfn, x) == query_hash(hashfn, copy(x))
+    end
 end
