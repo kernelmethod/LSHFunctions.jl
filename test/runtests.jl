3a70784d72f96d346f3139d9a0f4bc86b1278183
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-01-12 01:42:44 -0700

    Move the LSHTable and LSHTableGroup tests into test/tables.

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,5 +28,5 @@ include("test_lshfunction.jl")
 include(joinpath("function_hashing", "test_monte_carlo.jl"))
 include(joinpath("function_hashing", "test_chebhash.jl"))
 
-include("test_table.jl")
-include("test_table_group.jl")
+include(joinpath("tables", "test_table.jl"))
+include(joinpath("tables", "test_table_group.jl"))
