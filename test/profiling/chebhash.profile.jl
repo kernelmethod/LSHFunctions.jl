8b7a0ff83730fd17b08ab05cef6f8eb16d1d57f0
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-01-14 14:17:15 -0700

    Add profiling for ChebHash.

---
+++ test/profiling/chebhash.profile.jl
@@ -0,0 +1,30 @@
+using Random, Profile
+using LSH: ChebHash, cossim
+
+include(joinpath("..", "utils.jl"))
+
+#========================
+Benchmarks / profiling
+========================#
+
+Random.seed!(0)
+Profile.init(delay = 1e-3, n = 100_000)
+
+# Pre-compile
+hashfn = ChebHash(cossim, 1024)
+let f = ShiftedSine(π, π * rand())
+    f(randn()); f.(randn(5))
+end
+
+# Start generating hash functions and computing hashes
+Profile.clear_malloc_data()
+
+for ii = 1:10
+    f = ShiftedSine(π, π * rand())
+    @profile hashfn(f)
+end
+
+open("chebhash.benchmark.prof", "w") do f
+    Profile.print(f)
+    println("Results written to ", f.name)
+end
