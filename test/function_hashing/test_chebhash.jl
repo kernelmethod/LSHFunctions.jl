0f262c13ba44966c21c231bd43984440cd685cc6
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-01-14 11:21:53 -0700

    (Temporarily) remove the nontrivial input ChebHash tests and add more thorough trivial input tests.

--- test/function_hashing/test_chebhash.jl
+++ test/function_hashing/test_chebhash.jl
@@ -29,43 +29,34 @@ Tests
     Cosine similarity hashing
     ==========#
     @testset "Hash cosine similarity with trivial inputs" begin
-        # Hash over cosine similarity with two functions with cosine similarity
-        # zero. Their collision rate should be close to 50%.
-        f(x) = (-1.0 ≤ x ≤ 0.0) ? 1.0 : 0.0;
-        g(x) = (-1.0 ≤ x ≤ 0.0) ? 0.0 : 1.0;
+        ### Hash inputs with cosine similarity -1
+        f(x) = sin(x)
+        g(x) = -f(x)
         hashfn = ChebHash(cossim, 1024)
+
+        @test embedded_similarity(hashfn, f, g) ≈ -1
+
+        hf, hg = hashfn(f), hashfn(g)
+        @test mean(hf .== hg) == 0
+
+        ### Hash inputs with cosine similarity 0
+        # Note: f(x)g(x) = sin(x)cos(x) = 0.5 sin(2x), so that cosine
+        # similarity is zero (over the interval [-1,1]).
+        f(x) = sin(x)
+        g(x) = cos(x)
 
         @test embedded_similarity(hashfn, f, g) ≈ 0
 
         hf, hg = hashfn(f), hashfn(g)
         @test 0.45 ≤ mean(hf .== hg) ≤ 0.55
-    end
 
-    @testset "Hash cosine similarity with nontrivial inputs" begin
-        # Test hashing on cosine similarity using step functions defined
-        # over [0,N].
-        N = 10
-        interval = LSH.@interval(0.0 ≤ x ≤ N)
-        hashfn = ChebHash(cossim, 1024; interval=interval)
+        ### Hash inputs with cosine similarity 1
+        f(x) = sin(x)
+        g(x) = 2f(x)
 
-        @test let success = true
-            for ii = 1:128
-                f, f_steps = create_step_function(N)
-                g, g_steps = create_step_function(N)
+        @test embedded_similarity(hashfn, f, g) ≈ 1
 
-                prob = LSH.single_hash_collision_probability(hashfn, cossim(f_steps, g_steps))
-                hf, hg = hashfn(f), hashfn(g)
-
-                success &= (prob-0.05 ≤ mean(hf .== hg) ≤ prob+0.05)
-
-                if !success
-                    break
-                end
-                success
-            end
-        end
+        hf, hg = hashfn(f), hashfn(g)
+        @test mean(hf .== hg) == 1
     end
 end
-
-
-
