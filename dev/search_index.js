7ff4d9ad319b6a38044c32755f6a1712332e2fba
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-16 22:43:19 +0000

    build based on 172d0a1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LSH.jl-1","page":"Home","title":"LSH.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for the LSH.jl package.","category":"page"}]
+}
